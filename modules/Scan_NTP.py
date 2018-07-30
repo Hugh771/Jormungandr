206cb79bf5aceb569ebd5f63a8272d8b14800071
Lty <937617839@qq.com>
2018-07-30 18:46:01 +0800

    update

--- modules/Scan_NTP.py
+++ modules/Scan_NTP.py
@@ -5,7 +5,7 @@ import time
 import queue
 
 err_q=queue.Queue()
-mdb=pymongo.MongoClient()
+mdb=pymongo.MongoClient().scan.ntp
 
 def scan():
     global err_q
@@ -27,10 +27,24 @@ def scan():
 
         if m.all_hosts() != []:
             for IP in m.all_hosts():
-                n.scan(hosts=IP,ports='U:123',arguments='-sU -Pn -n --script=ntp-monlist')
+                try:
+                    n.scan(hosts=IP,ports='U:123',arguments='-sU -Pn -n --script=ntp-monlist')
+                except:
+                    try:
+                        n.scan(hosts=IP,ports='U:123',arguments='-sU -Pn -n --script=ntp-monlist')
+                    except:
+                        continue
                 if type(n[IP]['udp'][123].get('script')) == dict:
                     if n[IP]['udp'][123]['script'].get('ntp-monlist') != None:
                         if len(n[IP]['udp'][123]['script']['ntp-monlist'])>400:
-                            
-
+                            mdb.insert_one({'IP':IP})
+    f=open('./err_q_ip.txt','rw+')
+    while not err_q.empty():
+        ips=err_q.get()
+        f.write(ips+'\n')
+        print(ips)
+    f.close()
+                              
+if __name__ == '__main__':
+    scan()                       
 
